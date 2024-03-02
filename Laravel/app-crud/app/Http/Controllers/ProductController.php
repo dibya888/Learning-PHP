23973cb26dd5de0649e9dd54e7e016d72268d926
dibya888 <dibya.dhar@northsouth.edu>
2024-03-02 21:46:49 +0600

    Created Product Page

--- Laravel/app-crud/app/Http/Controllers/ProductController.php
+++ Laravel/app-crud/app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -13,6 +14,16 @@ class ProductController extends Controller
         return view('products.create'); //Show Create Form in Products Folder. Location: Laravel/app-crud/resources/views/products/create.blade.php
     }
     public function store(Request $request){
-        dd($request->name);    //dd=Data Dump. To check if the data is being sent to the controller
+        //dd($request->name);    //dd=Data Dump. To check if the data is being sent to the controller
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
+
+        $newProduct = Product::create($data);
+
+        return redirect (route('product.index'));
     }
 }
--- Laravel/app-crud/resources/views/products/create.blade.php
+++ Laravel/app-crud/resources/views/products/create.blade.php
@@ -8,6 +8,16 @@
 <body>
     <!-- Product Create Page -->
     <h1>Create a product</h1>
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+
+            @endforeach
+        </ul>
+        @endif
+    </div>
     <form method="post" action="{{route('product.store')}}">
         @csrf
         @method('post')
