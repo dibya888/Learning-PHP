7a9fe60fc11897a9ee729d055e2e051974ec7d90
dibya888 <dibya.dhar@northsouth.edu>
2024-03-19 11:39:46 +0600

    Code beautification

--- Laravel/app-crud/resources/views/products/create.blade.php
+++ Laravel/app-crud/resources/views/products/create.blade.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <!-- Product Create Page -->
     <h1>Create a product</h1>
     <div>
-        @if($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-
-            @endforeach
-        </ul>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         @endif
     </div>
-    <form method="post" action="{{route('product.store')}}">
+    <form method="post" action="{{ route('product.store') }}">
         @csrf
         @method('post')
         <div>
@@ -42,4 +43,5 @@
         </div>
     </form>
 </body>
+
 </html>
